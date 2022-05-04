3302a1fd4d16ca7fd85d5d80bef11c798e2fd7fd
Paul Mercer <paul.mercer@qa.com>
2022-05-04 12:13:33 +0100

    More changes

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -9,7 +9,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,7 +17,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-//sample change
 app.UseAuthorization();
 
 app.MapRazorPages();
